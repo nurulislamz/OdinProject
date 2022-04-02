44f80beefe2eebcfa69a2830c430543b08a9c7e2
nurulislamz <mohammed19.islam@gmail.com>
2022-04-02 17:32:27 +0100

    Finished

--- Second/script.js
+++ Second/script.js
@@ -5,14 +5,14 @@
 4)  calculate if the player or the computer won 
 5)  
 */
+let user_wins = 0;
+let computer_wins = 0;
 
 const data = {
     "rock": "paper",
     "paper": "scissors",
     "scissors": "paper",
-}
-
-// 1) get user input
+};
 
 // test if input is valid
 function validateInput(input){
@@ -26,35 +26,7 @@ function validateInput(input){
     }
 }
 
-// function that activates whenever the user inputs a value
-const form_submit = document.getElementById("submit");
-
-form_submit.addEventListener("click", function(event) {
-    event.preventDefault();
-    user_input = document.getElementById("user_choice").value;
-    computer_input = computer_selection()
-    console.log(user_input);
-
-    if (validateInput(user_input)){
-        document.getElementById("user_output").innerText = "You picked: " + user_input;
-        document.getElementById("cc_output").innerHTML = "Computer picked: " + computer_input
-
-        if (get_result(user_input, computer_input) == "draw"){
-            document.getElementById("result").innerText = "Draw!"
-        }
-        else if (get_result(user_input, computer_input) == "user wins"){
-            document.getElementById("result").innerText = "You win!"
-        }
-        else {
-            document.getElementById("result").innerText = "Computer win!"
-        }
-    }
-    else {
-        alert("Invalid input")
-    }
-} )
-
-// 2) display computer input
+// display computer input
 
 function computer_selection(){
     let random_choice = Math.floor(Math.random() * Object.keys(data).length);
@@ -64,7 +36,8 @@ function computer_selection(){
     return computer_choice
 }
 
-// 3) calculate if player or computer won
+
+// calculate if player or computer won
 function get_result(user_input, computer_input){
     if(user_input == computer_input){
         return "draw";
@@ -77,3 +50,38 @@ function get_result(user_input, computer_input){
     }
 }
 
+
+// function that activates whenever the user inputs a value
+const form_submit = document.getElementById("submit");
+
+form_submit.addEventListener("click", function(event) {
+    // gets user and player inputs
+    event.preventDefault();
+    user_input = document.getElementById("user_choice").value;
+    computer_input = computer_selection();
+    console.log(user_input);
+
+    // if the input is valid, updates html to show inputs
+    if (validateInput(user_input)){
+        document.getElementById("user_output").innerText = "You picked: " + user_input;
+        document.getElementById("cc_output").innerHTML = "Computer picked: " + computer_input;
+
+        // displays who wons and increments score counter 
+        if (get_result(user_input, computer_input) == "draw"){
+            document.getElementById("result").innerText = "Draw!";
+        }
+        else if (get_result(user_input, computer_input) == "user wins"){
+            document.getElementById("result").innerText = "You win!";
+            user_wins += 1;
+            document.getElementById("user_wins").innerText = "User wins: " + user_wins
+        }
+        else {
+            document.getElementById("result").innerText = "Computer win!";
+            computer_wins += 1;
+            document.getElementById("computer_wins").innerText = "Computer wins: " + computer_wins
+        }
+    }
+    else {
+        alert("Invalid input")
+    }
+} )
