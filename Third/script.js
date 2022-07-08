53278e5b524350f740b626b609e4bd1aa59e9410
root <root@DESKTOP-BGV52E7.localdomain>
2022-07-08 18:08:26 +0100

    updated

--- Third/script.js
+++ Third/script.js
@@ -1,1 +1,20 @@
-let div = document.createElement("div")
+function grid () {
+    const container = document.getElementById("container");
+    let squares = document.getElementById("squares");
+    console.log(squares)
+    // creates 16 grid rows of size 1fr
+    container.style.gridTemplateRows = 'repeat(16, 1fr)';
+    // creates 16 grid columns of size 1fr
+    container.style.gridTemplateColumns = 'repeat(16, 1fr)';
+
+    for (let i=0; i<=256; i++){
+        let newDiv = document.createElement("div");
+        newDiv.style.backgroundColor = "blue";
+        newDiv.addEventListener("mouseover", () => {
+            newDiv.style.backgroundColor = "black";
+        })
+        container.insertAdjacentElement("beforeend", newDiv)    
+    }
+}
+
+grid()
